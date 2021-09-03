c701632a3d39873aa08fa66da12e34b3fa380233
AM-ssfs <89938084+AM-ssfs@users.noreply.github.com>
2021-09-03 15:27:51 -0400

    Update FourOctogons.py

--- FourOctogons.py
+++ FourOctogons.py
@@ -3,17 +3,17 @@ Import turtle
 x = 0
 y = 0
 
-While(x<4):	#makes 4 of the shapes
+While(x<4): 	  #makes 4 of the shapes
 
-  While(y<8):	#makes a 8 sided shape
+  While(y<8): 	#makes an octogon
 
-    turtle.foward(25)
-    turtle.right(22.5)
-    y = y+1
+    turtle.foward(25)     #side length 25 units
+    turtle.right(22.5)    #rotates 180/8 deg
+    y = y + 1             #when this happens 8 times the loop will break
 
   turtle.right(90)		#rotates turtle so ready for next shape
-  turtle.foward(80)	#turtle moves so octagons not overlap
-  y = 0
-  x = x + 1
+  turtle.foward(80)	  #turtle moves so octagons not overlap
+  y = 0        #makes the octogon loop run again
+  x = x + 1    #when this happens 4 times the loop will break
   
 turtle.ExitOnClick()
