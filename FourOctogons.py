f26df3a8603d0fb7e37ed6f0c1e5de0a8faca7b7
AM-ssfs <89938084+AM-ssfs@users.noreply.github.com>
2021-09-03 15:29:06 -0400

    Update FourOctogons.py

--- FourOctogons.py
+++ FourOctogons.py
@@ -11,7 +11,7 @@ While(x<4): 	 #makes 4 of the shapes
     turtle.right(22.5)    #rotates 180/8 deg
     y = y + 1             #when this happens 8 times the loop will break
 
-  turtle.right(90)	#rotates turtle so ready for next shape
+  turtle.right(90)	  #rotates turtle so ready for next shape
   turtle.foward(80)	  #turtle moves so octagons not overlap
   y = 0        #makes the octogon loop run again
   x = x + 1    #when this happens 4 times the loop will break
