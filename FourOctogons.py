79f6e8ed384f3e45132c1ada98bdff6bcbed2aa9
AM-ssfs <89938084+AM-ssfs@users.noreply.github.com>
2021-09-03 15:19:46 -0400

    Create FourOctogons.py

---
+++ FourOctogons.py
@@ -0,0 +1,18 @@
+Import turtle
+
+x = 0
+y = 0
+
+While(x<4):	#makes 4 of the shapes
+
+  While(y<8):	#makes 8 sided shape
+
+    turtle.foward(25)
+    turtle.right(22.5)
+    y = y+1
+
+  turtle.right(90)		#rotates turtle so ready for next shape
+  turtle.foward(80)	#turtle moves so octagons not overlap
+  y = 0
+  x = x + 1
+turtle.ExitOnClick()
