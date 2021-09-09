4aab2e2724d79bd5073804afb04f36c05dc5e79b
AM-ssfs <menscad@ssfs.org>
2021-09-09 11:06:39 -0400

    more stuff (2)

--- shapes.py
+++ shapes.py
@@ -5,25 +5,27 @@ turtle.colormode(255)
 turtle.pensize(1)
 
 
+
 def poly(sides, size):
     for x in range(sides):
-        turtle.forward(size)
+        turtle.forward(size/2)
         turtle.right(360/sides)
 
 
 def thing(size):
-    for x in range(15):
+    for x in range(20):
         poly(x+2, size)
+        turtle.pencolor((0 + x * 11 ), 0, (255 - x * 8))
 
 
 def something(sides, size):
     for x in range(sides):
         thing(size)
-        turtle.forward(size)
-        turtle.right(360/sides)
+        turtle.forward(size/2)
+        turtle.left(360/sides)
 
 
-something(6, 25)
+something(5, 60)
 
 turtle.penup()
 turtle.exitonclick()
