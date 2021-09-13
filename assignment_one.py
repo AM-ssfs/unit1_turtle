01e9a579f1a170e555f776c9a447eb9c049c2758
AM-ssfs <menscad@ssfs.org>
2021-09-13 13:59:49 -0400

    function creates houses of different sizes and colors

---
+++ assignment_one.py
@@ -0,0 +1,27 @@
+import turtle
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+
+def octagon(color):
+
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+
+    for x in range(8):
+
+        turtle.forward(25)
+        turtle.right(45)
+
+    turtle.end_fill()
+
+
+for x in range(1, 5):
+
+    octagon(colors[x])
+    turtle.penup()
+    turtle.goto(0+x*50, (0+x*50))
+    turtle.pendown()
+
+turtle.exitonclick()
